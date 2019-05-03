ca2dae4ed2bb8db081304c4d2875aa83fc2b2234
christopher.coulson <christopher.coulson@higpagesgroup.com.au>
2019-05-03 17:40:20 +1000

    feat: refactor to use correct package name feat: adding command line parsers to configure object

--- Dockerfile
+++ Dockerfile
@@ -15,14 +15,14 @@ FROM base AS test
 
 COPY tests/requirements.txt ${HYPERKOPS_HOME}/tests/requirements.txt
 RUN  pip install -r ${HYPERKOPS_HOME}/tests/requirements.txt
-COPY monitor monitor
+COPY hyperkops/monitor monitor
 COPY launcher launcher
 COPY tests tests
 RUN py.test -v --cov=.  --cov-config .coveragerc
 
 FROM base AS run
 
-COPY monitor monitor
+COPY hyperkops/monitor monitor
 COPY launcher launcher
 COPY run.sh run.sh
 
--- hyperkops/monitor/trials_timeout_monitor.py
+++ hyperkops/monitor/trials_timeout_monitor.py
@@ -1,37 +1,27 @@
 import datetime
-import os
 import logging as log
+import os
 from time import sleep
+
 from hyperopt import JOB_STATE_RUNNING, JOB_STATE_ERROR
 from hyperopt.utils import coarse_utcnow
-from monitor.mongo_db_utils import MongodbConnection
 
 
-class MongodbTrialsTimeoutMonitor(MongodbConnection):
+class MongodbTrialsTimeoutMonitor:
     """Queries the table hyperopt uses to store state and prints the results"""
-    log.basicConfig(level=os.environ.get("LOGLEVEL"), format='%(asctime)s %(levelname)s %(message)s')
+    log.basicConfig(level=os.environ.get("LOGLEVEL"),
+                    format='%(asctime)s %(levelname)s %(message)s')
 
     def __init__(self,
-                 mongo_db_address=None,
-                 mongo_db_port=None,
-                 timeout_interval=None,
-                 update_interval=None):
-        self._timeout_interval = timeout_interval if timeout_interval is not None else int(
-            os.environ['TRIALS_TIMEOUT_INTERVAL'])
-        self._db_name = "model_db"
-        self._collection_name = "jobs"
-        self._update_interval = update_interval if update_interval is not None else int(
-            os.environ['UPDATE_INTERVAL'])
+                 timeout_interval,
+                 update_interval,
+                 mongodb_connection):
 
-        super().__init__(mongo_db_address, mongo_db_port)
+        self._timeout_interval = timeout_interval
+        self._update_interval = update_interval
+        self._mongodb_connection = mongodb_connection
 
-    def _init_jobs_collection(self):
-        """Initialise connection to jobs collection
-        :return None
-        """
-        self.init_connection_to_collection(self._db_name, self._collection_name)
-
-    def error_stale_jobs(self):
+    def monitor_for_stale_jobs(self):
         """Hyperopt is designed to be deployed within a stable computing environment where the underlying instances
         are long lived. It is therefore designed around graceful failure of the worker units. If a worker fails
         through a python exception it emits a shutdown failure message to mongodb, and sets all of it current jobs to a
@@ -41,7 +31,6 @@ class MongodbTrialsTimeoutMonitor(MongodbConnection):
         This monitor helps out Hyperopt by timing out jobs which are long-lived and therefore likely associated
         with jobs which were running on pods which have been killed"""
 
-        self._init_jobs_collection()
         while True:
             log.info("Checking for stale jobs")
             cur_timeout_boundary = self._get_current_timeout_boundary(self._timeout_interval)
@@ -54,13 +43,13 @@ class MongodbTrialsTimeoutMonitor(MongodbConnection):
 
             # Query the Mongo DB for jobs which extend past teh timeout bounday, and iterate through the doucments
             # upserting them to an error state
-            for trial in self._collection.find(
+            for trial in self._mongodb_connection.collection.find(
                     {'state': JOB_STATE_RUNNING, 'refresh_time': {'$lt': cur_timeout_boundary}}):
                 self._log_warning_of_error(trial['_id'], trial['tid'], trial['owner'])
                 # Update the state of the job to an error
                 trial['state'] = JOB_STATE_ERROR
                 # Upsert the job into mongodb
-                self._collection.replace_one({'_id': trial['_id']}, trial, True)
+                self._mongodb_connection.collection.replace_one({'_id': trial['_id']}, trial, True)
                 counter = counter + 1
             log.info("Stale job check complete")
             log.info("Number of stale jobs found :: " + str(counter))
@@ -83,10 +72,3 @@ class MongodbTrialsTimeoutMonitor(MongodbConnection):
         log.warning("Forcing Job to error state due to timeout: \n" + warning_detail)
 
 
-
-
-if __name__ == "__main__":
-    print("Starting logging")
-    log.info("Starting logging")
-    mongo_jobs_logger = MongodbTrialsTimeoutMonitor()
-    mongo_jobs_logger.error_stale_jobs()
