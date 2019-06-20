341c557f2248d9a7a3dad0568b172a6a4b073915
christopher.coulson <christopher.coulson@higpagesgroup.com.au>
2019-06-20 17:02:19 +1000

    fix: revert to master

--- hyperkops/worker/kube_worker.sh
+++ hyperkops/worker/kube_worker.sh
@@ -7,24 +7,18 @@
 
 echo "Getting Environmental Variables"
 
-mongo_db_address=${MONGO_DB_ADDRESS:-localhost}
-mongo_db_port=${MONGO_DB_PORT:-27017}
-trials_db=${TRIALS_DB:-model_db}
-
-
 # Create MongoDB Address from environmental variables
-mongo_db_full_qualified_address="${mongo_db_address}:${mongo_db_port}/${TRIALS_DB}"
+mongo_db_address="${MONGO_DB_ADDRESS}:${MONGO_DB_PORT}/${TRIALS_DB}"
 
-echo "MongoDB Address ${mongo_db_full_qualified_address}"
-## Create a string of arguments using the environmental variables
-argument_string="hyperopt-mongo-worker --mongo ${mongo_db_full_qualified_address} "
+echo "MongoDB Adderss ${mongo_db_address}"
+## Create string of argument using the environmental variables
+argument_string="hyperopt-mongo-worker --mongo ${mongo_db_address} "
 
 for var in "${!HYPEROPT_@}"; do
     # Remove prefix and make the environmental variable lowercase
     argument=$(echo "${var}" | sed -e "s/^HYPEROPT_//" | sed "s/_/-/" | tr '[:upper:]' '[:lower:]')
     argument_string="${argument_string}--${argument} ${!var} "
 done
-
 echo "Starting Hyperopt worker"
 
 eval "${argument_string}"
