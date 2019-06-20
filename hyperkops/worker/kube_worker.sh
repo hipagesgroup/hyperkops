82df1268b5996ceeb30e958e8266a48eec7ab14f
christopher.coulson <christopher.coulson@higpagesgroup.com.au>
2019-06-20 14:37:59 +1000

    fix: changing ways env vars are read to work on linux

--- hyperkops/worker/kube_worker.sh
+++ hyperkops/worker/kube_worker.sh
@@ -7,18 +7,23 @@
 
 echo "Getting Environmental Variables"
 
+extra_arguments=""
+
+for var in "$(env | awk -F "=" '{print $1}' | grep "HYPEROPT_ARG_.*")"; do
+    # Remove prefix and make the environmental variable lowercase
+    argument=$(echo "${var}" | sed -e "s/HYPEROPT_ARG_//" | sed "s/_/-/" | tr '[:upper:]' '[:lower:]')
+    extra_arguments="${extra_arguments}--${argument} ${!var} "
+done
+
+echo "Starting Hyperopt worker"
+
 # Create MongoDB Address from environmental variables
 mongo_db_address="${MONGO_DB_ADDRESS}:${MONGO_DB_PORT}/${TRIALS_DB}"
 
 echo "MongoDB Adderss ${mongo_db_address}"
 ## Create string of argument using the environmental variables
-argument_string="hyperopt-mongo-worker --mongo ${mongo_db_address} "
+argument_string="hyperopt-mongo-worker --mongo ${mongo_db_address} ${extra_arguments}"
 
-for var in "${!HYPEROPT_@}"; do
-    # Remove prefix and make the environmental variable lowercase
-    argument=$(echo "${var}" | sed -e "s/^HYPEROPT_//" | sed "s/_/-/" | tr '[:upper:]' '[:lower:]')
-    argument_string="${argument_string}--${argument} ${!var} "
-done
-echo "Starting Hyperopt worker"
+echo $argument_string
 
 eval "${argument_string}"
