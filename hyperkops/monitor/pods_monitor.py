4562f2f97c233faedccc1777f6f5332c8deffca0
christopher.coulson <christopher.coulson@higpagesgroup.com.au>
2019-06-20 18:18:52 +1000

    fix: updating naming

--- hyperkops/monitor/pods_monitor.py
+++ hyperkops/monitor/pods_monitor.py
@@ -23,21 +23,21 @@ class PodMonitor:
         """
         running_pods = self.kube_api_connector.get_running_pods(self.label_selector)
 
-        query_results = self.get_running_trials()
-        pods_running_trials = self.get_pods_running_trials(query_results)
+        trials_list = self.get_running_trials()
+        pods_running_trials = self.get_pods_running_trials(trials_list)
         deleted_pods = set(pods_running_trials) - set(running_pods)
 
-        self.upsert_jobs_on_deleted_pods(deleted_pods, query_results)
+        self.upsert_jobs_on_deleted_pods(deleted_pods, trials_list)
 
-    def upsert_jobs_on_deleted_pods(self, deleted_pods, query_results):
+    def upsert_jobs_on_deleted_pods(self, deleted_pods, trials_list):
         """
         Upserts records where the pod is no-longer running
         :param deleted_pods: List of string names of the deleted pods
-        :param query_results: list of dictionaries of the currently running trials
+        :param trials_list: list of dictionaries of the currently running trials
         :return: None
         """
         counter = 0
-        for trial in query_results:
+        for trial in trials_list:
             if self.get_pod_name_from_owner_string(trial['owner'][0]) in list(deleted_pods):
                 trial['state'] = JOB_STATE_ERROR
                 log.warning("Trial deleted from MongoDB owner = " + str(trial['owner'][0]))
@@ -47,17 +47,17 @@ class PodMonitor:
 
         log.info("Number of stale jobs detected : " + str(counter))
 
-    def get_pods_running_trials(self, query_results):
+    def get_pods_running_trials(self, trials_list):
         """
         Uses the results to find the unique names of pods running trials
-        :param query_results: List of dictionaries, each dictionary is a running trial
+        :param trials_list: List of dictionaries, each dictionary is a running trial
         :return: list of string pod names
         """
 
         log.debug("Running Trials ::")
 
         owner_counters = Counter()
-        for trial in query_results:
+        for trial in trials_list:
             owner_counters[self.get_pod_name_from_owner_string(trial['owner'][0])] += 1
             log.debug(trial)
         pods_running_trials = list(owner_counters.keys())
